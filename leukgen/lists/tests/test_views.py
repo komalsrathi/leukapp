401738e88f2ffcef4cb28cf13fc1eabf40c75476
Juan Medina <jsmedmar@gmail.com>
2015-09-14 12:27:24 -0400

    Lists app home page HTML is now generated from a template

--- leukgen/lists/tests/test_views.py
+++ leukgen/lists/tests/test_views.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from ..views import lists_page
+from ..views import home_page
 
 # Create your tests here.
 
@@ -12,11 +12,11 @@ class ListsPageTest(TestCase):
 
     def test_root_url_resolves_to_lists_page_view(self):
         found = resolve('/lists/')
-        self.assertEqual(found.func, lists_page)
+        self.assertEqual(found.func, home_page)
 
     def test_lists_home_page_returns_correct_html(self):
         request = HttpRequest()
-        response = lists_page(request)
+        response = home_page(request)
         expected_html = render_to_string('lists/list_home.html')
         self.assertEqual(response.content.decode(), expected_html)
         self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
