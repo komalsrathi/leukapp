755da30c7445673c237deca5a851dc9a72fd0b23
Juan Medina <jsmedmar@gmail.com>
2015-09-14 11:14:33 -0400

    adds templatefolder for lists app

--- leukgen/lists/views.py
+++ leukgen/lists/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def lists_page(request):
-    return HttpResponse('<html><title>TO-DO lists</title></html>')
+    return render(request, 'lists_home.html')
--- tests/functional_tests/functional_tests.py
+++ tests/functional_tests/functional_tests.py
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000/lists')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('TO-DO lists', self.browser.title)
+        self.assertIn('To-Do lists', self.browser.title)
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -40,10 +40,10 @@ class NewVisitorTest(unittest.TestCase):
         self.assertTrue(
                 any(row.text == '1: Buy peacock feathers' for row in rows)
         )
-        self.fail('Finish the test!')
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
         # Edith wonders whether the site will remember her list. Then she sees
