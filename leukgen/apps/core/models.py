fb188454ec0f1ae342387cef9c4fb21a6d697b33
Juan Medina <jsmedmar@gmail.com>
2015-10-02 21:50:01 -0400

    adds LuekappModel.slug update_on_save functionality

--- leukgen/apps/core/models.py
+++ leukgen/apps/core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
 
 
 class TimeStampedModel(models.Model):
@@ -18,9 +20,55 @@ class TimeStampedModel(models.Model):
 class LeukappModel(TimeStampedModel):
 
     """
-    An abstract base class model that provides self-updating
-    ``created`` and ``modified`` fields.
+    Leukapp Base Model
     """
+
+    ext_id = models.CharField(
+        _("External ID"),
+        max_length=100
+        )
+    slug = models.CharField(
+        _("Slug"),
+        max_length=100,
+        blank=True
+        )
 
     class Meta:
         abstract = True
+
+    def save(self, *args, **kwargs):
+        """
+        slug requires pk for new objects
+        """
+
+        new = not self.pk
+        super(LeukappModel, self).save(*args, **kwargs)
+
+        # http://stackoverflow.com/questions/9940674/django-model-manager-objects-create-where-is-the-documentation
+
+        if new:
+            kwargs['force_insert'] = False  # set to avoid in error in create()
+            self.slug = self.get_slug()
+            super(LeukappModel, self).save(*args, **kwargs)
+
+        else:
+            self.slug = self.get_slug()
+            super(LeukappModel, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse(
+            self.APP_NAME + ':detail', kwargs={'slug': self.slug}
+        )
+
+    def get_update_url(self):
+        return reverse(
+            self.APP_NAME + ':update', kwargs={'slug': self.slug}
+        )
+
+    def get_create_url(self):
+        return reverse(
+            self.APP_NAME + ':create', kwargs={'slug': self.slug}
+        )
+
+    def get_slug(self, **kwargs):
+        raise NotImplementedError("Implement me")
