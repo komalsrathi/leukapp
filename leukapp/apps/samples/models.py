a21facb3bdf1c21252de4032aa46c8814c446a7e
Juan Medina <jsmedmar@gmail.com>
2015-10-09 19:11:53 -0400

    creates sample app to manage sample submisions

---
+++ leukapp/apps/samples/models.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# leukapp
+from leukapp.apps.individuals.models import IndividualAbstractModel
+from leukapp.apps.specimens.models import SpecimenAbstractModel
+from leukapp.apps.aliquots.models import AliquotAbstractModel
+
+
+class PseudoIndividual(IndividualAbstractModel):
+
+    """
+    Main PseudoIndividual Model
+    """
+
+
+class PseudoSpecimen(SpecimenAbstractModel):
+
+    """
+    Main PseudoSpecimen Model
+    """
+
+
+class PseudoAliquot(AliquotAbstractModel):
+
+    """
+    Main PseudoAliquot Model
+    """
