4ab6767c9d79258137318a21705391b56ba30c39
Juan Medina <jsmedmar@gmail.com>
2015-10-09 11:53:56 -0400

    adds tests for custom validators

---
+++ leukapp/apps/core/tests/test_validators.py
@@ -0,0 +1,22 @@
+# django imports
+from django.test import TestCase
+from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
+
+# local
+from ..validators import ext_id_validator
+
+
+class CoreValidatorsTest(TestCase):
+
+    def test_ext_id_validator_doesnt_allow_white_spaces(self):
+        with self.assertRaises(ValidationError):
+            ext_id_validator("Juan San")
+
+    def test_ext_id_validator_displays_correct_msg(self):
+        msg = "Enter a valid 'External id' consisting of" \
+            " letters, numbers, underscores or hyphens."
+        try:
+            ext_id_validator("Juan San")
+        except ValidationError as e:
+            self.assertEqual(e.message, _(msg))
--- leukapp/apps/core/validators.py
+++ leukapp/apps/core/validators.py
@@ -1,19 +1,29 @@
 from django.core.validators import RegexValidator
+from django.utils.translation import ugettext_lazy as _
 
-phone = RegexValidator(
+phone_validator = RegexValidator(
     regex=r'^\+?1?\d{9,15}$',
-    message="Phone number must be entered in the format: '+999999999'." +
-        "Up to 15 digits allowed."
+    message=_("Phone number must be entered in the format: '+999999999'."
+        "Up to 15 digits allowed.")
     )
 
-name = RegexValidator(
+name_validator = RegexValidator(
     regex=r'^[a-zA-Z\s]$',
-    message="Use under scores."
+    message=_("Use under scores.")
     )
 
-object_name = RegexValidator(
+object_name_validator = RegexValidator(
     regex=r'^[-a-zA-Z0-9_\s]+\Z',
-    message="Enter a valid name consisting of letters, numbers, white spaces"
-        ", underscores or hyphens.",
-    code='invalid'
+    code='invalid',
+    message=_("Enter a valid name consisting of letters, numbers, white spaces"
+        ", underscores or hyphens."),
+    )
+
+# the ext_id_validator uses the same regex as
+# django.core.validators.validate_slug
+ext_id_validator = RegexValidator(
+    regex=r'^[-a-zA-Z0-9_]+\Z',
+    code='invalid',
+    message=_("Enter a valid 'External id' consisting of"
+        " letters, numbers, underscores or hyphens."),
     )
