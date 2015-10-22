dda527d76953c3be1372cd7c832ef80588970ff3
Juan Medina <jsmedmar@gmail.com>
2015-10-22 11:47:31 -0400

    starts working on deployment fabfiles

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,8 +1,11 @@
-from fabric.contrib.files import append, exists, sed
-from fabric.api import env, local, run
+# python
 import random
 
-REPO_URL = 'https://github.com/jsmedmar/leukapp'
+# third party
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
+
+REPO_URL = 'git@github.com:leukgen/leukapp.git'
 
 
 def deploy():
@@ -17,7 +20,7 @@ def deploy():
 
 
 def _create_directory_structure_if_necessary(site_folder):
-    for subfolder in ('source'):
+    for subfolder in ['source']:
         run('mkdir -p %s/%s' % (site_folder, subfolder))
 
 
@@ -31,40 +34,27 @@ def _get_latest_source(source_folder):
 
 
 def _update_settings(source_folder, site_name):
-    # settings_path = source_folder + '/superlists/settings.py'
-    # sed(settings_path, "DEBUG = True", "DEBUG = False")  #1
-    # sed(settings_path,
-    #     'ALLOWED_HOSTS =.+$',
-    #     'ALLOWED_HOSTS = ["%s"]' % (site_name,)  #2
-    # )
-    # secret_key_file = source_folder + '/superlists/secret_key.py'
-    # if not exists(secret_key_file):  #3
-    #     chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-    #     key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
-    #     append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
-    # append(settings_path, '\nfrom .secret_key import SECRET_KEY')  #45
+    """ not needed because we have different settings files """
     pass
 
 
 def _update_virtualenv(source_folder):
-    # virtualenv_folder = source_folder + '/../virtualenv'
-    # if not exists(virtualenv_folder + '/bin/pip'): #1
-    #     run('virtualenv --python=python3 %s' % (virtualenv_folder,))
-    # run('%s/bin/pip install -r %s/requirements.txt' % ( #2
-    #         virtualenv_folder, source_folder
-    # ))
-    pass
+    # check if virtualenv exists
+    virtualenv_folder = '/home/medinaj/.virtualenvs/staging'
+    if not exists(virtualenv_folder):
+        run('mkvirtualenv staging')
+
+    # Install requirements:
+    requirements = source_folder + '/requirements/production.txt'
+    run('%s/bin/pip install -r %s' % (virtualenv_folder, requirements))
 
 
 def _update_static_files(source_folder):
-    # run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic --noinput' % ( # 1
-    #     source_folder,
-    # ))
+    """this one is not needed because we are Django White Noise"""
     pass
 
 
 def _update_database(source_folder):
-    # run('cd %s && ../virtualenv/bin/python3 manage.py migrate --noinput' % (
-    #     source_folder,
-    # ))
-    pass
+    python = '/home/medinaj/.virtualenvs/staging/bin/python'
+    command = 'cd {0} && {1} manage.py migrate --noinput'
+    run(command.format(source_folder, python))
