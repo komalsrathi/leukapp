55ed53a193710d551eee368f287136d628d5d2ae
Juan Medina <jsmedmar@gmail.com>
2015-11-04 18:06:35 -0500

    refactor: factories is now completely tested and has delete=False/True functionality

--- leukapp/apps/leukforms/factories.py
+++ leukapp/apps/leukforms/factories.py
@@ -11,62 +11,58 @@ import io
 from django.core.exceptions import ImproperlyConfigured
 from django.conf import settings
 
-# leukapp
-from leukapp.apps.projects.factories import ProjectFactory
-from leukapp.apps.individuals.factories import IndividualFactory
-from leukapp.apps.specimens.factories import SpecimenFactory
-from leukapp.apps.aliquots.factories import AliquotFactory
-from leukapp.apps.runs.factories import RunFactory
-from leukapp.apps.individuals.constants import INDIVIDUAL_LEUKFORM_FIELDS
-from leukapp.apps.specimens.constants import SPECIMEN_LEUKFORM_FIELDS
-from leukapp.apps.aliquots.constants import ALIQUOT_LEUKFORM_FIELDS
-from leukapp.apps.runs.constants import RUN_LEUKFORM_FIELDS
-
 # local
-from .constants import LEUKFORM_CSV_FIELDS
+from .constants import LEUKFORM_FIELDS, LEUKAPP_FACTORIES
 
 
-class LeukformCsvFactory(object):
+class LeukformSamplesFactory(object):
 
     """
-    Class used as a runs factory.
+    Class used as a leukform samples factory.
 
     Attributes:
-        individuals (list): list of all individuals created
-        specimens (list): list of all specimens created
-        aliquots (list): list of all aliquots created
-        rows (list of dictionaries): simulates the leukform
+        instances (dict): keys are models, values are created instances
+        request (dict): keys are models, values are requested instances
+        delete (boolean): whether or not instances are deleted at the end
+        rows (list): each element is a valid leukform dictionary
     Methods:
-        create_batch: creates a batch of runs
-        get_rows: creates `rows` simulating leukform based on batch
-        _create_csv_from_rows: creates csv from rows
-        _create_stringio_from_rows: creates StringIO from rows
+        create_batch: creates a batch of samples and returns rows
+        get_rows: returns the rows shuffled
+        create_csv_from_rows: creates csv from rows
+        create_stringio_from_rows: creates StringIO from rows
     """
 
-    leukform_fields = {
-        'Individual': INDIVIDUAL_LEUKFORM_FIELDS,
-        'Specimen': SPECIMEN_LEUKFORM_FIELDS,
-        'Aliquot': ALIQUOT_LEUKFORM_FIELDS,
-        'Run': RUN_LEUKFORM_FIELDS,
-        }
-
     def __init__(self):
-        super(LeukformCsvFactory, self).__init__()
-        self.individuals = []
-        self.specimens = []
-        self.aliquots = []
-        self.runs = []
+        super(LeukformSamplesFactory, self).__init__()
+
+        # delete created instances
+        self.delete = True
+
+        # initialize rows
         self.rows = []
-        self.row = {}
-        self.projects = [ProjectFactory(name=str(i)) for i in range(10)]
+        self._row = {}
+
+        # created instances
         self.instances = {
-            'Individual': self.individuals,
-            'Specimen': self.specimens,
-            'Aliquot': self.aliquots,
-            'Run': self.runs,
+            'Individual': [],
+            'Specimen': [],
+            'Aliquot': [],
+            'Run': [],
             }
 
-    def create_batch(self, individuals=2, specimens=3, aliquots=2, runs=2,
+        # requested instances
+        self.request = {
+            'Individual': 0,
+            'Specimen': 0,
+            'Aliquot': 0,
+            'Run': 0,
+            }
+
+        # projects used
+        ProjectFactory = LEUKAPP_FACTORIES['Project']
+        self.projects = [ProjectFactory(name=str(i)) for i in range(10)]
+
+    def create_batch(self, individuals, specimens=0, aliquots=0, runs=0,
             delete=True):
         """
         Creates a batch of runs.
@@ -79,82 +75,105 @@ class LeukformCsvFactory(object):
         Raises:
             ImproperlyConfigured if batch has already been created
         """
-        if self.individuals:
-            msg = "Batch has already been created, run .__init__() to reset"
-            raise ImproperlyConfigured(msg)
+        if self.instances['Individual']:
+            self.__init__()
 
         self.delete = delete
-        self.last = False
+        self.request = {
+            'Individual': individuals,
+            'Specimen': specimens,
+            'Aliquot': aliquots,
+            'Run': runs,
+            }
+
         self.i = individuals
         self.s = specimens
         self.a = aliquots
         self.r = runs
 
-        self.rows = []
-        self._create_individuals()
-        if delete:
-            [i.delete() for i in self.individuals]
+        self._create_instances(model='Individual')
+
+        if self.delete:
+            [i.delete() for i in self.instances['Individual']]
+
         return self.rows
 
     def get_rows(self, ordered=True):
-        """ return rows """
+        """ if ordered, returns shuffled rows; else, returns rows """
         if not ordered:
             random.shuffle(self.rows)
         return self.rows
 
-    def _create_individuals(self):
-        """ NOTTESTED NOTDOCUMENTED """
-        for NOTUSED in range(self.i):
-            self.row = {}
-            i, self.last = IndividualFactory(), (self.s == 0)
-            self._write_row(i, 'Individual')
-            self.individuals.append(i)
-            self._create_specimens(i)
+    def _create_instances(self, model, parent=None):
+        """
+        Recursively generates instance passing its model and parent.
+        This function calls self._set_parameters, which provides
+        model specific kwargs and the name of the child model
 
-    def _create_specimens(self, i):
-        """ NOTTESTED NOTDOCUMENTED """
-        for order in range(self.s):
-            s = SpecimenFactory(individual=i, order=order)
-            self.last = (self.a == 0)
-            self._write_row(s, 'Specimen')
-            self.specimens.append(s)
-            self._create_aliquots(s)
+        Input:
+            model (str): name of instance's model
+            parent (Leukapp Instance): foreing key's object
+        """
+        for order in range(self.request[model]):
+            kwargs, child = self._set_parameters(model, parent, order)
+            instance = LEUKAPP_FACTORIES[model](**kwargs)
+            self.instances[model].append(instance)
+            self._write_row(instance, model)
+            if child:
+                self._create_instances(model=child, parent=instance)
 
-    def _create_aliquots(self, s):
-        """ NOTTESTED NOTDOCUMENTED """
-        for NOTUSED in range(self.a):
-            a, self.last = AliquotFactory(specimen=s), (self.r == 0)
-            self._write_row(a, 'Aliquot')
-            self.aliquots.append(a)
-            self._create_runs(a)
+    def _set_parameters(self, model, parent, order=None):
+        """
+        Provides the model specific Factory kwargs. Also determines whether
+        or not this istance is in the last level of creation by quering
+        the number of requested instances for the child model.
 
-    def _create_runs(self, a):
-        """ NOTTESTED NOTDOCUMENTED """
-        for NOTUSED in range(self.r):
-            ps = random.sample(self.projects, 3)
-            pl = '|'.join(str(p.pk) for p in ps)
-            r = RunFactory(aliquot=a, projects=ps)
-            r.projects_list = pl
-            self.last = True
-            self._write_row(r, 'Run')
-            self.runs.append(r)
+        Input:
+            model (str): name of instance's model
+            parent (Leukapp Instance): foreing key's object
+            order (int): number of instances created so far for the given model
+        Return:
+            (kwargs (dict), child (str)): kawrgs is the model specific creation
+                parameters, while child is the name of the child model
+        """
+        if model == 'Individual':
+            self._last = (self.request['Specimen'] == 0)
+            return {}, 'Specimen'
+        if model == 'Specimen':
+            self._last = (self.request['Aliquot'] == 0)
+            return {'individual': parent, 'order': str(order)}, 'Aliquot'
+        if model == 'Aliquot':
+            self._last = (self.request['Run'] == 0)
+            return {'specimen': parent}, 'Run'
+        if model == 'Run':
+            self._last = True
+            pl = '|'.join(str(p.pk) for p in random.sample(self.projects, 3))
+            return {'aliquot': parent, 'projects_list': pl}, None
 
     def _write_row(self, instance, model):
-        """ NOTTESTED NOTDOCUMENTED """
-        if (not self.delete) and (not self.last):
-            self.row = {}
-            self.row[model + '.slug'] = instance.slug
-        else:
-            for field in self.leukform_fields[model]:
-                column = "%s.%s" % (model, field)
-                value = 'instance.%s' % field
-                self.row[column] = eval(value)
-        if self.last:
-            instance.delete()
-            self.rows.append(self.row)
-        return self.row
+        """
+        Writes each instance data to self._row and appends it to self.rows
+        when self._last is True. Instances in the last level of creation will
+        always be deleted. If self.delete and self._last are False only the
+        instance slug will be recorded.
 
-    def _create_csv_from_rows(self):
+        Input:
+            model (str): name of instance's model
+            parent (Leukapp Instance): foreing key's object
+        """
+        if (not self.delete) and (not self._last):
+            self._row = {}
+            self._row[model + '.slug'] = instance.slug
+        else:
+            for field in LEUKFORM_FIELDS[model]:
+                column = "{0}.{1}".format(model, field)
+                value = eval('instance.{0}'.format(field))
+                self._row[column] = value
+        if self._last:
+            instance.delete()
+            self.rows.append(self._row.copy())
+
+    def create_csv_from_rows(self):
         """
         Creates a csv from rows.
         Returns: Path of csv
@@ -163,7 +182,7 @@ class LeukformCsvFactory(object):
         if not self.rows:
             raise ImproperlyConfigured("create rows first")
 
-        timestamp = datetime.datetime.now().isoformat()
+        timestamp = datetime.datetime.now().isoformat().split('.')[0]
         file_name = 'test_leukform_' + timestamp + '.csv'
         path = os.path.join(settings.MEDIA_ROOT, 'csv', 'outrows', file_name)
         keys = set(self.rows[0])
@@ -175,7 +194,7 @@ class LeukformCsvFactory(object):
 
         return path
 
-    def _create_stringio_from_rows(self):
+    def create_stringio_from_rows(self):
         """
         Creates a string from rows.
         Returns: stringio
