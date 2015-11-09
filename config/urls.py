8b1f1bfa07b1a8d9e20b8e2db658a473afd2f32e
Juan Medina <jsmedmar@gmail.com>
2015-11-09 15:01:28 -0500

    upgrades to django 1.9

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views import defaults
 
 from .views import ConfigTemplateView
 
@@ -86,13 +87,3 @@ urlpatterns = [
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
-    urlpatterns += [
-        url(r'^400/$', 'django.views.defaults.bad_request'),
-        url(r'^403/$', 'django.views.defaults.permission_denied'),
-        url(r'^404/$', 'django.views.defaults.page_not_found'),
-        url(r'^500/$', 'django.views.defaults.server_error'),
-    ]
