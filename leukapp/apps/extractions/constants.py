31dc2ab3137321424975bb99c124155c49681c79
Juan Medina <jsmedmar@gmail.com>
2015-11-25 19:15:26 -0500

    extractions: sequencing code update plus constants tests.

--- leukapp/apps/extractions/constants.py
+++ leukapp/apps/extractions/constants.py
@@ -14,16 +14,16 @@ here.
 from leukapp.apps.core import constants as coreconstants
 
 # APP INFO
-# -----------------------------------------------------------------------------
+# =============================================================================
 APP_NAME = 'extractions'  #: Application's name.
 
 # URLS
-# -----------------------------------------------------------------------------
+# =============================================================================
 EXTRACTION_CREATE_URL = APP_NAME + ':create'  #: Create URL reverse string.
 EXTRACTION_LIST_URL = APP_NAME + ':list'      #: List URL reverse string.
 
 # FIELDS
-# -----------------------------------------------------------------------------
+# =============================================================================
 
 #: Fields required to create a new instance.
 EXTRACTION_CREATE_FIELDS = (
@@ -43,85 +43,32 @@ EXTRACTION_UPDATE_FIELDS = tuple()
 EXTRACTION_UNIQUE_TOGETHER = tuple()
 
 # CHOICES
-# -----------------------------------------------------------------------------
-WHOLE_EXOME = 'WHOLE-EXOME'
-WHOLE_GENOME = 'WHOLE-GENOME'
-TARGETED_DNA = 'TARGETED-DNA'
-RNA_SEQ_TARGETED = 'RNA-SEQ-TARGETED'
-RNA_SEQ_WHOLE_TRANSCRIPTOME = 'RNA-SEQ-WHOLE-TRANSCRIPTOME'
-RNA_SEQ_SINGLE_CELL = 'RNA-SEQ-SINGLE-CELL'
-DNA_METHYLATION_SEQ = 'DNA-METHYLATION-SEQ'
-CHIP_SEQ = 'CHIP-SEQ'
-ATAC_SEQ = 'ATAC-SEQ'
-TECHNOLOGY = (
-    (WHOLE_EXOME, 'WHOLE-EXOME'),
-    (WHOLE_GENOME, 'WHOLE-GENOME'),
-    (TARGETED_DNA, 'TARGETED-DNA'),
-    (RNA_SEQ_TARGETED, 'RNA-SEQ-TARGETED'),
-    (RNA_SEQ_WHOLE_TRANSCRIPTOME, 'RNA-SEQ-WHOLE-TRANSCRIPTOME'),
-    (RNA_SEQ_SINGLE_CELL, 'RNA-SEQ-SINGLE-CELL'),
-    (DNA_METHYLATION_SEQ, 'DNA-METHYLATION-SEQ'),
-    (CHIP_SEQ, 'CHIP-SEQ'),
-    (ATAC_SEQ, 'ATAC-SEQ'),
-    )
-"""
-List of value, verbose_name pairs for the
-:attr:`~leukapp.apps.extractions.models.Extraction.technology` attribute.
-"""
+# =============================================================================
 
-TECHNOLOGY_VALUE = [e[0] for e in TECHNOLOGY]
-"""
-Values for the :attr:`~leukapp.apps.extractions.models.Extraction.technology`
-attribute.
-"""
-
-AGILENTV4 = 'AGILENTV4'
-AGILENTV5 = 'AGILENTV5'
-WHOLEGENOMELIBRARYV1 = 'WHOLEGENOMELIBRARYV1'
-HEMEPACTV1 = 'HEMEPACTV1'
-HEMEPACTV2 = 'HEMEPACTV2'
-PLATFORM = (
-    (AGILENTV4, 'AGILENTV4'),
-    (AGILENTV5, 'AGILENTV5'),
-    (WHOLEGENOMELIBRARYV1, 'WHOLEGENOMELIBRARYV1'),
-    (HEMEPACTV1, 'HEMEPACTV1'),
-    (HEMEPACTV2, 'HEMEPACTV2'),
-    )
-"""
-List of value, verbose_name pairs for the
-:attr:`~leukapp.apps.extractions.models.Extraction.platform` attribute.
-"""
-
-PLATFORM_VALUE = [e[0] for e in PLATFORM]
-"""
-Values for the :attr:`~leukapp.apps.extractions.models.Extraction.platform`
-attribute.
-"""
-
+# SEQUENCING CENTER
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 CMO = 'CMO'
 NYGC = 'NYGC'
 FOUNDATION = 'FOUNDATION'
+
 CENTER = (
-    (CMO, 'CMO'),
-    (NYGC, 'NYGC'),
-    (FOUNDATION, 'FOUNDATION'),
+    (CMO, CMO),
+    (NYGC, NYGC),
+    (FOUNDATION, FOUNDATION),
     )
 """
 List of value, verbose_name pairs for the
 :attr:`~leukapp.apps.extractions.models.Extraction.center` attribute.
 """
 
-CENTER_VALUE = [e[0] for e in CENTER]
-"""
-Values for the :attr:`~leukapp.apps.extractions.models.Extraction.center`
-attribute.
-"""
-
+# ANALYTE
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 DNA = 'DNA'
 RNA = 'RNA'
+
 ANALYTE = (
-    (DNA, 'DNA'),
-    (RNA, 'RNA'),
+    (DNA, DNA),
+    (RNA, RNA),
     )
 """
 List of value, verbose_name pairs for the
@@ -134,6 +81,162 @@ Values for the :attr:`~leukapp.apps.extractions.models.Extraction.analyte`
 attribute.
 """
 
+# TECHNOLOGY
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+WHOLEGENOME = 'WHOLE-GENOME'
+WHOLEEXOME = 'WHOLE-EXOME'
+TARGETEDDNA = 'TARGETED-DNA'
+RNASEQ = 'RNA-SEQ'
+RNASEQCUSTOM = 'RNA-SEQ-CUSTOM'
+RNASEQSINGLECELL = 'RNA-SEQ-SINGLE-CELL'
+CHIPSEQ = 'CHIP-SEQ'
+ATACSEQ = 'ATAC-SEQ'
+FOUNDATION = 'FOUNDATION'
+RLP = 'RLP'
+
+TECHNOLOGY = (
+    (WHOLEEXOME, WHOLEEXOME),
+    (WHOLEGENOME, WHOLEGENOME),
+    (TARGETEDDNA, TARGETEDDNA),
+    (RNASEQ, RNASEQ),
+    (RNASEQCUSTOM, RNASEQCUSTOM),
+    (RNASEQSINGLECELL, RNASEQSINGLECELL),
+    (CHIPSEQ, CHIPSEQ),
+    (ATACSEQ, ATACSEQ),
+    (FOUNDATION, FOUNDATION),
+    (RLP, RLP),
+    )
+"""
+List of `(value, verbose name)` pairs for the
+:attr:`~leukapp.apps.extractions.models.Extraction.technology` attribute.
+"""
+
+# PLATFORM
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+# WHOLE GENOME
+HISEQ = "HISEQ"
+ILLUMINAXTEN = "ILLUMINA-XTEN"
+
+# WHOLE EXOME
+AGILENT50MB = "AGILENT-50MB"
+AGILENT51MB = "AGILENT-51MB"
+
+# RNA-SEQ
+PAIREDEND50BP = "PAIRED-END-50BP"
+SINGLEEND150BP = "SINGLE-END-150BP"
+
+# TARGETED DNA
+HEMEPACTV1 = "HEMEPACT-V1"
+HEMEPACTV2 = "HEMEPACT-V2"
+HEMEPACTV3 = "HEMEPACT-V3"
+IMPACTHEME = "IMPACT-HEME"
+IMPACT300 = "IMPACT-300"
+IMPACT340 = "IMPACT-340"
+IMPACTCLINICAL = "IMPACT-CLINICAL"
+
+# CHIP-SEQ
+H3K4ME1 = "H3K4ME1"
+H3K4ME3 = "H3K4ME3"
+H3K4ME2 = "H3K4ME2"
+H3K27AC = "H3K27AC"
+
+# FOUNDATION
+FOUNDATIONONEHEMEPANEL = "FOUNDATIONONE-HEME-PANEL"
+FOUNDATIONONEPANEL = "FOUNDATIONONE-PANEL"
+
+PLATFORM = (
+    (HISEQ, HISEQ),
+    (ILLUMINAXTEN, ILLUMINAXTEN),
+    (AGILENT50MB, AGILENT50MB),
+    (AGILENT51MB, AGILENT51MB),
+    (PAIREDEND50BP, PAIREDEND50BP),
+    (SINGLEEND150BP, SINGLEEND150BP),
+    (HEMEPACTV1, HEMEPACTV1),
+    (HEMEPACTV2, HEMEPACTV2),
+    (HEMEPACTV3, HEMEPACTV3),
+    (IMPACTHEME, IMPACTHEME),
+    (IMPACT300, IMPACT300),
+    (IMPACT340, IMPACT340),
+    (IMPACTCLINICAL, IMPACTCLINICAL),
+    (H3K4ME1, H3K4ME1),
+    (H3K4ME3, H3K4ME3),
+    (H3K4ME2, H3K4ME2),
+    (H3K27AC, H3K27AC),
+    (FOUNDATIONONEHEMEPANEL, FOUNDATIONONEHEMEPANEL),
+    (FOUNDATIONONEPANEL, FOUNDATIONONEPANEL),
+    )
+"""
+List of value, verbose_name pairs for the
+:attr:`~leukapp.apps.extractions.models.Extraction.platform` attribute.
+"""
+
+# TECHNOLOGY-PLATFORM CODES
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+TECHNOLOGY_PLATFORM = {
+
+    WHOLEGENOME: {
+        HISEQ: "1",
+        ILLUMINAXTEN: "1",
+        },
+
+    WHOLEEXOME: {
+        AGILENT50MB: "2",
+        AGILENT51MB: "2",
+        },
+
+    RNASEQ: {
+        PAIREDEND50BP: "3",
+        SINGLEEND150BP: "3",
+        },
+
+    TARGETEDDNA: {
+        HEMEPACTV1: "4",
+        HEMEPACTV2: "4",
+        HEMEPACTV3: "4",
+        IMPACTHEME: "4",
+        IMPACT300: "5",
+        IMPACT340: "5",
+        IMPACTCLINICAL: "5",
+        },
+
+    CHIPSEQ: {
+        H3K4ME1: "",
+        H3K4ME3: "",
+        H3K4ME2: "",
+        H3K27AC: "",
+        },
+
+    RLP: {
+        "": "8",
+        },
+
+    RNASEQCUSTOM: {
+        "": "9",
+        },
+
+    ATACSEQ: {
+        "": "10",
+        },
+
+    RNASEQSINGLECELL: {
+        "": "11",
+        },
+
+    FOUNDATION: {
+        FOUNDATIONONEHEMEPANEL: "12",
+        FOUNDATIONONEPANEL: "12",
+        },
+
+    }
+"""
+Technology platform codes dictionary.
+
+.. important: Juan doesn't like this "code" thing. Too artistic.
+"""
+
+# ALL CHOICES
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 EXTRACTION_CHOICES = {
     "PLATFORM": PLATFORM,
     "TECHNOLOGY": TECHNOLOGY,
@@ -145,6 +248,8 @@ Dictionary including all
 :class:`Extraction's <~leukapp.apps.extractions.models.Extraction>` choices.
 """
 
+# LEUKID ANALYTE CHARACTERS
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 LEUKID_ANALYTE = {
     DNA: 'D',
     RNA: 'R',
@@ -155,7 +260,7 @@ Characters used in the **leukid** to describe the `Extraction's`
 """
 
 # PERMISSIONS
-# -----------------------------------------------------------------------------
+# =============================================================================
 
 #: Tuple of permissions required to create a new instance.
 EXTRACTION_CREATE_PERMISSIONS = ('extractions.add_extraction',)
@@ -164,7 +269,7 @@ EXTRACTION_CREATE_PERMISSIONS = ('extractions.add_extraction',)
 EXTRACTION_UPDATE_PERMISSIONS = ('extractions.change_extraction',)
 
 # MESSAGES
-# -----------------------------------------------------------------------------
+# =============================================================================
 
 #: Sucess message.
 SUCCESS_MESSAGE = coreconstants.SUCCESS_MESSAGE
---
+++ leukapp/apps/extractions/tests/test_constants.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests for the `extractions` application `constants`.
+"""
+
+# django
+from django.test import TestCase
+
+# local
+from .. import constants
+
+
+class ExtractionsConstantsTest(TestCase):
+
+    def test_all_platforms_are_in_techplatcode_dictionary(self):
+        """
+        All platforms must be also included only once in TECHNOLOGY_PLATFORM.
+        """
+        PLATFORM = [e[0] for e in constants.PLATFORM]
+
+        for platform in PLATFORM:
+            platform_isin_codedict = 0
+            for key in constants.TECHNOLOGY_PLATFORM:
+                if platform in constants.TECHNOLOGY_PLATFORM[key]:
+                    platform_isin_codedict += 1
+            self.assertEqual(True, platform_isin_codedict == 1)
+
+    def test_all_analytes_must_be_in_LEUKID_ANALYTE(self):
+        """
+        All analytes in ANALYTE must have a leukid character.
+        """
+        ANALYTE = [e[0] for e in constants.ANALYTE]
+        for analyte in ANALYTE:
+            self.assertIn(analyte, constants.LEUKID_ANALYTE)
