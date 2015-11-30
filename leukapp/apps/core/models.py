e026257ae715ab968a2da90d0cbbbd00ece2b4df
Juan Medina <jsmedmar@gmail.com>
2015-11-30 16:00:15 -0500

    core: adds documentation to core models

--- leukapp/apps/core/models.py
+++ leukapp/apps/core/models.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+"""
+Abstract models used across the :mod:`leukapp` project.
+"""
+
 # python
 import inspect
 
@@ -28,51 +32,82 @@ class TimeStampedModel(models.Model):
 class LeukappModel(TimeStampedModel):
 
     """
-    Leukapp Base Model
+    **Leukapp** abstract base model.
+
+    This model has several methods that are shared across the **Leukapp**
+    project. This implementation is mainly motivated by the DRY concept.
     """
 
+    # META CLASS
+    # =========================================================================
     class Meta:
         abstract = True
 
+    # PUBLIC METHODS
+    # =========================================================================
     @transaction.atomic
     def save(self, *args, **kwargs):
         """
-        slug requires pk for new objects
+        `Atomic`_ method to save the :class:`LeukappModel`.
+
+        While executed, calls two methods:
+
+            * :meth:`_if_new`: only called at creation, used to generate IDs.
+            * :meth:`_if_save`: called every time the object is saved.
+
+        The ``slug`` identification system is based on the use of primary keys.
+        As such, the :class:`LeukappModel` must be saved before generating the
+        ``slug``.
+
+        The keyword argument ``force_insert`` is set to ``False`` in order to
+        avoid error while using :meth:`create` (see `this question`_).
+
+        .. _this question: http://stackoverflow.com/questions/9940674/django-model-manager-objects-create-where-is-the-documentation
+        .. _Atomic: https://docs.djangoproject.com/en/1.8/topics/db/transactions/#controlling-transactions-explicitly
         """
 
-        new = not self.pk
-
-        # http://stackoverflow.com/questions/9940674/django-model-manager-objects-create-where-is-the-documentation
-
-        if new:
+        if not self.pk:
             super(LeukappModel, self).save(*args, **kwargs)  # get pk
             kwargs['force_insert'] = False  # set to avoid error in create()
             self._if_new()
             self._if_save()
-
         else:
             self._if_save()
 
         super(LeukappModel, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
-        """ Returns the object detail url. """
+        """
+        Returns the object detail url.
+        """
         return reverse(self.APP_NAME + ':detail', kwargs={'slug': self.slug})
 
     def get_update_url(self):
-        """ Returns the object's update url. """
+        """
+        Returns the object's update url.
+        """
         return reverse(self.APP_NAME + ':update', kwargs={'slug': self.slug})
 
+    # PRIVATE METHODS
+    # =========================================================================
+
     def _if_new(self, **kwargs):
-        """ _if_new is executed the first time the object is created. """
+        """
+        :meth:`_if_new` is executed the first time the object is created.
+        """
         pass
 
     def _if_save(self, **kwargs):
-        """ _if_save is executed everytime the object is saved. """
+        """
+        :meth:`_if_save` is executed everytime the object is saved.
+        """
         pass
 
     def _check_if_caller_is_if_new(self):
-        """ Checks if current function is been called from _if_new(). """
+        """
+        Checks if current function is been called from :meth:`_if_new`.
+        This ensures the atomicity of the IDs generation.
+        """
 
         # NOTTESTED
         msg = "This function can only be called from _if_new()."
@@ -83,9 +118,11 @@ class LeukappModel(TimeStampedModel):
             raise(Exception(msg))
 
     def _check_if_caller_is_save(self):
-        """ Checks if current function is been called from save(). """
+        """
+        Checks if current method is been called from :meth:`save`.
+        This ensures the atomicity of the IDs generation.
+        """
 
-        # NOTTESTED
         msg = "This function can only be called from save()."
         try:
             if inspect.stack()[2][3] != 'save':
@@ -97,13 +134,16 @@ class LeukappModel(TimeStampedModel):
 class LeukappTestModel(LeukappModel):
 
     """
-    this model only serves the purpose of testing the abstract models above
+    This class only serves the purpose of testing the core's abstract models.
     """
 
     def _if_save(self):
-        """
-        _if_save is executed everytime the object is saved
-        test: test_str_returns_slug
-        """
-
+        self._check_if_caller_is_save()
         self.slug = self.pk
+
+    def _if_new(self):
+        self._check_if_caller_is_save()
+        self._test()
+
+    def _test(self):
+        self._check_if_caller_is_if_new()
