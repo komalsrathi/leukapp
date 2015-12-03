bb7b5b6be1e5b539b581c67877b6e8196272ad8c
Juan Medina <jsmedmar@gmail.com>
2015-12-03 18:02:12 -0500

    leukforms: adds Workflow form

--- leukapp/apps/leukforms/forms.py
+++ leukapp/apps/leukforms/forms.py
@@ -3,15 +3,19 @@
 # django
 from django import forms
 
-# leukapp
+# leukapp create fields
 from leukapp.apps.individuals.constants import INDIVIDUAL_CREATE_FIELDS
 from leukapp.apps.specimens.constants import SPECIMEN_CREATE_FIELDS
 from leukapp.apps.aliquots.constants import ALIQUOT_CREATE_FIELDS
 from leukapp.apps.extractions.constants import EXTRACTION_CREATE_FIELDS
+from leukapp.apps.workflows.constants import WORKFLOW_CREATE_FIELDS
+
+# leukapp models
 from leukapp.apps.individuals.models import Individual
 from leukapp.apps.specimens.models import Specimen
 from leukapp.apps.aliquots.models import Aliquot
 from leukapp.apps.extractions.models import Extraction
+from leukapp.apps.workflows.models import Workflow
 
 
 class IndividualForm(forms.ModelForm):
@@ -37,9 +41,13 @@ class AliquotForm(forms.ModelForm):
 
 class ExtractionForm(forms.ModelForm):
 
-    def __init__(self, *args, **kwargs):
-        super(ExtractionForm, self).__init__(*args, **kwargs)
-
     class Meta:
         model = Extraction
         fields = EXTRACTION_CREATE_FIELDS
+
+
+class WorkflowForm(forms.ModelForm):
+
+    class Meta:
+        model = Workflow
+        fields = WORKFLOW_CREATE_FIELDS
